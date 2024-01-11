9471f610224a46e1c0f0e53ab85f23545674fa13
Arul Murugavel <63643748+BLooDBRothER@users.noreply.github.com>
2024-01-11 13:51:15 +0530

    Add status endpoint

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	
+	server := gin.Default()
+
+	server.GET("/status", serverStatus)
+
+	server.Run();
+}
+
+func serverStatus(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Server is running..."})
 }
